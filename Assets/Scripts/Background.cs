587df6ee8e4f3a60a03f3673d9981d60a0d7f57d
Zukky55 <www.kase@gmail.com>
2018-09-01 15:09:25 +0900

    commit

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    [SerializeField] float m_scrollSpeed = 1.0f;
+
+    /// <summary>
+    /// スクロール速度。正の値の時には下へスクロールする。負の値の時には上へスクロールする
+    /// </summary>
+    public float ScrollSpeed
+    {
+        get { return m_scrollSpeed; }
+        set { m_scrollSpeed = value; }
+    }
+
+    void Update()
+    {
+        // Time.deltaTime を使って滑らかに縦にスクロールさせる
+        transform.Translate(Vector2.left * Time.deltaTime * m_scrollSpeed);
+    }
+}
